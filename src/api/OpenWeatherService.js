2097076da7f75b6c4de1f919a577666dec5f6bb8
Hemant Paliwal <112302360+Sarthak052@users.noreply.github.com>
2024-11-27 16:21:45 +0530

    Update OpenWeatherService.js

--- src/api/OpenWeatherService.js
+++ src/api/OpenWeatherService.js
@@ -1,7 +1,7 @@
 const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
-const WEATHER_API_KEY = 'Your API KEY';
+const WEATHER_API_KEY = 'b190a0605344cc4f3af08d0dd473dd25';
 
 const GEO_API_OPTIONS = {
   method: 'GET',
