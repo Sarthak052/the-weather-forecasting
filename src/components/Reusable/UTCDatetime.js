55a8180fc652bdc04f12c5530647a4e70755ea18
Hemant Paliwal <112302360+Sarthak052@users.noreply.github.com>
2024-12-08 23:18:11 +0530

    Update UTCDatetime.js

--- src/components/Reusable/UTCDatetime.js
+++ src/components/Reusable/UTCDatetime.js
@@ -2,20 +2,9 @@ import { Typography } from '@mui/material';
 import React from 'react';
 import { getUTCDatetime } from '../../utilities/DatetimeUtils';
 
-const ISTDatetime = () => {
-  const getISTDatetime = () => {
-    const utcDate = new Date(getUTCDatetime()); // Get UTC datetime
-    const istOffset = 5.5 * 60; // IST offset in minutes (UTC + 5:30)
-    
-    // Adjust the date object for IST
-    utcDate.setMinutes(utcDate.getMinutes() + istOffset);
-    
-    return utcDate.toISOString().replace('T', ' ').substring(0, 19); // Format as 'YYYY-MM-DD HH:MM:SS'
-  };
-
-  const istFullDate = getISTDatetime();
-  
-  const istTimeValue = (
+const UTCDatetime = () => {
+  const utcFullDate = getUTCDatetime();
+  const utcTimeValue = (
     <Typography
       variant="h3"
       component="h3"
@@ -28,11 +17,10 @@ const ISTDatetime = () => {
         fontFamily: 'Poppins',
       }}
     >
-      {istFullDate} IST
+      {utcFullDate} GMT
     </Typography>
   );
-
-  return istTimeValue;
+  return utcTimeValue;
 };
 
-export default ISTDatetime;
+export default UTCDatetime;
